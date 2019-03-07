18f403ae67648f7bb8576669d28571405122a1bd
Albert Mosialek <albert.mosialek@gmail.com>
2019-03-07 17:40:52 +0100

    added init, finalize, function to send multiple bytes

--- Lab1/homework1.c
+++ Lab1/homework1.c
@@ -6,44 +6,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mpi_send_test(int argc, char** argv)
+int world_rank;
+int world_size;
+
+void Init(char * argv[])
 {
     MPI_Init(NULL, NULL);
-    int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(world_rank==1)
-        for(int i=0;i<10000;i++)
-        {
-            printf("%s"," ");
-        }
-    // We are assuming at least 2 processes for this task
     if (world_size < 2) {
         fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
+}
+
+void Finalize()
+{
+    MPI_Finalize();
+}
+
+void mpi_send_test(int how_many_bytes) {
+    MPI_Barrier(MPI_COMM_WORLD);
+
     int number;
     if (world_rank == 0) {
         // If we are rank 0, set the number to -1 and send it to process 1
         number = -1;
         MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Send(&number, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
-        MPI_Send(&number, 1, MPI_INT, 3, 0, MPI_COMM_WORLD);
     } else if (world_rank == 1) {
         MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Process 1 received number %d from process 0\n", number);
     }
-    else if (world_rank == 2) {
-        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Process 2 received number %d from process 0\n", number);
+}
+int main(int argc, char* argv[])
+{
+    Init(argv);
+    double time;
+    for(i=0;i<10000000;i*=10)
+    {
+        time = mpi_send_test(i);
+        if(world_rank==0)
+            printf("process 0 has ended sending %d bytes after %f seconds\n",time);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if(world_rank==0)
+            printf("process 1 has ended receiving %d bytes after %f seconds\n",time);
     }
-    else if (world_rank == 3) {
-        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Process 3 received number %d from process 0\n", number);
-    }
-    MPI_Finalize();
-
-
+    Finalize();
 }
