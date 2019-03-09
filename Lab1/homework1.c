9161410e58c480f06048769db80a301a05edaf39
mosialek <mosialek@jabba.icsr.agh.edu.pl>
2019-03-10 00:31:23 +0100

    fix homework1

--- Lab1/homework1.c
+++ Lab1/homework1.c
@@ -28,31 +28,35 @@ void Finalize()
 double mpi_send_test(int how_many_ints) {
     MPI_Barrier(MPI_COMM_WORLD);
     double start,end;
-    int* numbers = malloc(how_many_ints* sizeof(int));
+    void* numbers = malloc(how_many_ints * sizeof(int));
+    //printf("%d %d\n",world_rank);
     start = MPI_Wtime();
     if (world_rank == 0) {
         // If we are rank 0, set the number to -1 and send it to process 1
 
-        MPI_Send(&numbers, how_many_ints, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(numbers, how_many_ints, MPI_INT, 1, 0, MPI_COMM_WORLD);
     } else if (world_rank == 1) {
-        MPI_Recv(&numbers, how_many_ints, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(numbers, how_many_ints, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         //printf("Process 1 received number %d from process 0\n", number);
     }
     end = MPI_Wtime();
     free(numbers);
+    return end-start;
 }
 int main(int argc, char* argv[])
 {
+    int provided;
     Init(argv);
+    //MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
     double time;
-    for(int i=0;i<1000;i*=10)
+    for(long long i=1;i<100;i+=1)
     {
-        time = mpi_send_test(i);
+        time = mpi_send_test(i*1000000);
+        //if(world_rank==0)
+        //    printf("process 0 has ended sending %d bytes after %f seconds\n",i,time);
+        //MPI_Barrier(MPI_COMM_WORLD);
         if(world_rank==0)
-            printf("process 0 has ended sending %d bytes after %f seconds\n",i,time);
-        MPI_Barrier(MPI_COMM_WORLD);
-        if(world_rank==0)
-            printf("process 1 has ended receiving %d bytes after %f seconds\n",i,time);
+            printf("%d %f\n",i,sizeof(int)*i/time);
     }
     Finalize();
 }
